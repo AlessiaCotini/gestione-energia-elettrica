4ce740e4574b0106527d34f7e7b5d0c0229e79cf
AlessiaCotini <alessia.cotini@icloud.com>
2026-07-20 13:21:11 +0200

    Initial commit

---
+++ src/main/java/gestione/elettrica/gestione_energia_elettrica/GestioneEnergiaElettricaApplication.java
@@ -0,0 +1,13 @@
+package gestione.elettrica.gestione_energia_elettrica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneEnergiaElettricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneEnergiaElettricaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestione/elettrica/gestione_energia_elettrica/GestioneEnergiaElettricaApplicationTests.java
@@ -0,0 +1,13 @@
+package gestione.elettrica.gestione_energia_elettrica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEnergiaElettricaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
