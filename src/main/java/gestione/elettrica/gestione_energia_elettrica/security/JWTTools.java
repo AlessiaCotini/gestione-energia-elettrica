13479cb91bd6dbcbda6b2d23c1cf57e6d3f1a314
AlessiaCotini <alessia.cotini@icloud.com>
2026-07-20 15:55:37 +0200

    update security class

---
+++ src/main/java/gestione/elettrica/gestione_energia_elettrica/security/JWTTools.java
@@ -0,0 +1,44 @@
+package gestione.elettrica.gestione_energia_elettrica.security;
+
+import gestione.elettrica.gestione_energia_elettrica.entities.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class JWTTools {
+    private final String secret;
+
+    public JWTTools(@Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
+    //AGGIUNGO I METODI A DISPOSIZIONE DELLA LIBRERIA PER GENERARE E VERIFICARE I TOKEN
+
+    // DATA EMISSIONE - DATA SCADENZA - ID DEL PROPRIETARIO - FIRMA DEL TOKEN
+
+    public String generoToken(User user){
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+ 1000 *60 *60 *24))
+                .subject(String.valueOf(user.getUserId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    public void verificoToken (String token){
+        try{
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(token);
+        } catch (Exception e) {
+            throw new RuntimeException("ERRORE TOKEN");
+        }
+    }
+
+    public UUID checkIdDalToken(String token){
+        return UUID.fromString(Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parseSignedClaims(token).getPayload().getSubject());
+    }
+}
---
+++ src/main/java/gestione/elettrica/gestione_energia_elettrica/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package gestione.elettrica.gestione_energia_elettrica.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder getBCrypt(){
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+
+        // DISABILITO IL FORM DI DEFAULT
+
+        httpSecurity.formLogin(formLogin -> formLogin.disable() );
+
+        //DISABILITO LE SESSIONI
+
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        //DISABILITIAMO ANCHE DA ATTACCHI CSRF
+
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        //ELIMINO I CONTROLLI AUTOMATICI DI SPRING SECURITY - quindi poi non interverrà sulle richieste
+
+        httpSecurity.authorizeHttpRequests(req -> req.requestMatchers("/auth/**").permitAll());
+
+        //IMPLEMENTO I MIEI CONTROLLI BASATI SUI TOKEN
+
+        return httpSecurity.build();
+
+    }
+}
