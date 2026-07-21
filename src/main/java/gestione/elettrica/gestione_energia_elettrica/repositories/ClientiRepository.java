0f28fb352d987256b7e006a326d9bcaadd1d6b3b
nicolorsillo <nicolorsillo@hotmail.it>
2026-07-21 11:46:16 +0200

    Created ClientiRepository

---
+++ src/main/java/gestione/elettrica/gestione_energia_elettrica/repositories/ClientiRepository.java
@@ -0,0 +1,22 @@
+package gestione.elettrica.gestione_energia_elettrica.repositories;
+
+import gestione.elettrica.gestione_energia_elettrica.entities.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ClientiRepository extends JpaRepository<Cliente, UUID> {
+
+    Page<Cliente> findByFatturatoAnnuale(double fatturatoAnnuale, Pageable pageable);
+
+    Page<Cliente> findByDataInserimento(LocalDate dataInserimento, Pageable pageable);
+
+    Page<Cliente> findByDataUltimoContatto(LocalDate dataUltimoContatto, Pageable pageable);
+
+    Page<Cliente> findByRagioneSocialeContaining(String ragioneSociale, Pageable pageable);
+
+}
