0cf03e1323a41afbeffaa4594828965ad502da8a
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-07-21 12:52:09 +0200

    csvService pt2

--- src/main/java/gestione/elettrica/gestione_energia_elettrica/GestioneEnergiaElettricaApplication.java
+++ src/main/java/gestione/elettrica/gestione_energia_elettrica/GestioneEnergiaElettricaApplication.java
@@ -1,13 +1,18 @@
 package gestione.elettrica.gestione_energia_elettrica;
 
+import gestione.elettrica.gestione_energia_elettrica.repositories.ComuneRepository;
+import gestione.elettrica.gestione_energia_elettrica.repositories.ProvinciaRepository;
+import gestione.elettrica.gestione_energia_elettrica.services.ImportCsvService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class GestioneEnergiaElettricaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestioneEnergiaElettricaApplication.class, args);
-	}
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(GestioneEnergiaElettricaApplication.class, args);
+    }
 
 }
